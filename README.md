d9129edf4ff1607de6f9127edec1fc2ee9b975d1
voidcode <terkelsorensen@gmail.com>
2014-07-19 23:32:28 +0000

    cleanup api.php

---
+++ README.md
@@ -0,0 +1,18 @@
+Installation steps for RestberryPi and Ubuntu:
+
+1)
+    Download the 'MyBooks'-project from GitHub 
+    
+        git clone git@hub
+2)
+    
+
+3)
+    Install imagemagick
+
+        sudo apt-get install imagemagick
+4)
+    Install imagemagick PECL extension
+    
+        sudo apt-get install php5-imagick
+
--- api.php
+++ api.php
@@ -3,6 +3,31 @@
 $PASSWORD = '1234';//<--'1234' is the default password
 
 //DO NO EDIT CODE BELOW
+$booksRoot = dirname(realpath(__FILE__)).'/books/';
+$coversRoot = dirname(realpath(__FILE__)).'/covers/';
+$booksArray = [];
+
+//local-functions
+function convertAllBooksToCovers(){
+    $handlers = opendir($booksArray);
+    while($file = readdir($handlers)){
+        if($file != '.' && $file != '..'){
+            $doc = new Imagick($file);
+            $data->getImageBlob();
+            file_put_contents($coversRoot.$file.'.png', $data);
+        }
+    }
+}
+function dirToArray($dir){
+    $booksArray = [];
+    $handler = opendir($dir);
+    while($filename = readdir($handler)){
+        if($filename !=='.' && $filename !== '..')
+        	array_push($booksArray, $filename);
+    }
+    return $booksArray;
+}
+//API-part
 switch($_GET['f']){
     case 'logon':
         if($_GET['password'] == $PASSWORD){
@@ -12,14 +37,10 @@ switch($_GET['f']){
         }
     break;
     case 'getBooks':
-        $filenameArray = [];
-        $booksRoot = dirname(realpath(__FILE__)).'/books/';
-        $handler = opendir($booksRoot);
-        while($file = readdir($handler)){
-        	if($file !=='.' && $file !== '..')
-        		array_push($filenameArray, $file);
-        }
-        echo json_encode($filenameArray);
+        echo json_encode(dirToArray($booksRoot));
+    break;
+    case 'convertAllBooksToCovers':
+        convertAllBooksToCovers();
     break;
 }
 ?>
